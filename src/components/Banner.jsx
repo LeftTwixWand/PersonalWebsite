95f8c78cdc2fa24c31f1399a15cb64d46d9051b5
Vladyslav Horbachov <vhorbachov@microsoft.com>
2022-12-14 13:31:22 +0200

    Added footer component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import { ArrowRightCircle } from "react-bootstrap-icons";
-// import "animate.css";
 import TrackVisibility from "react-on-screen";
+import "animate.css";
 
 export const Banner = () => {
   const [loopNum, setLoopNum] = React.useState(0);
